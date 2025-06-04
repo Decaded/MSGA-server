e1670d7489c05713aa838a47303ff8af1214f197
decaded <me@decaded.dev>
2025-06-05 01:18:56 +0200

    refactor: move securityHeaders middleware import to improve code organization

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -13,16 +13,7 @@
 const jwt = require('jsonwebtoken');
 const { env, errorMessages } = require('../config');
 const { getDatabase } = require('../utils/db');
-
-const securityHeaders = (req, res, next) => {
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'DENY');
-  res.setHeader(
-    'Content-Security-Policy',
-    "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self';"
-  );
-  next();
-};
+const { securityHeaders } = require('./securityHeaders');
 
 module.exports = function verifyToken(req, res, next) {
   securityHeaders(req, res, () => {
