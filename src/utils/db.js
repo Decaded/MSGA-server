00070cc85640cd03be70cdc02e4272b36870ead0
decaded <me@decaded.dev>
2025-06-05 22:29:24 +0200

    docs: update JSDoc comments for database utility functions in db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,23 @@
+/**
+ * Initializes the database by ensuring all required collections exist.
+ * Required collections: 'users', 'works', 'profiles', 'blockedTokens', 'webhooks'.
+ */
+
+/**
+ * Retrieves the data for the specified collection from the database.
+ * @param {string} name - The name of the collection to retrieve.
+ * @returns {*} The data stored in the specified collection.
+ */
+
+/**
+ * Sets the data for the specified collection in the database.
+ * @param {string} name - The name of the collection to set.
+ * @param {*} data - The data to store in the collection.
+ */
+
 const NyaDB = require('@decaded/nyadb');
 const db = new NyaDB();
 
-/**
- * Initializes the database by ensuring required collections exist.
- * Required collections: 'users', 'works'.
- */
 const initDB = () => {
   const required = ['users', 'works', 'profiles', 'blockedTokens', 'webhooks'];
   required.forEach(name => {
@@ -12,18 +25,8 @@ const initDB = () => {
   });
 };
 
-/**
- * Retrieves the data for the specified collection from the database.
- * @param {string} name - The name of the collection to retrieve.
- * @returns {*} The data stored in the specified collection.
- */
 const getDatabase = name => db.get(name);
 
-/**
- * Sets the data for the specified collection in the database.
- * @param {string} name - The name of the collection to update.
- * @param {*} data - The data to store in the collection.
- */
 const setDatabase = (name, data) => db.set(name, data);
 
 module.exports = { initDB, getDatabase, setDatabase };
